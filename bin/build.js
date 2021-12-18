05ec9e3d6363d1d6f19649fcc62f86eef06bbbd0
Lydia Goulding <lydia.p.goulding+git@gmail.com>
2021-12-17 20:22:36 -0600

    Create a script to build a tar ball for npm

---
+++ bin/build.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const tar = require('tar');
+const execa = require('execa');
+const packageJson = require('../package.json');
+
+const projectRoot = path.join(__dirname, '../');
+const srcRoot = path.join(projectRoot, 'src');
+
+const libRoot = path.join(projectRoot, 'lib');
+const esmRoot = path.join(libRoot, 'esm');
+
+const shell = (cmd) =>
+    execa(cmd, { stdio: ['pipe', 'pipe', 'inherit'], shell: true });
+
+const babel = (outDir, envName) =>
+    shell(
+        `npx babel ${srcRoot} --out-dir ${outDir} --env-name "${envName}"`,
+    );
+
+const createZip = () => {
+    tar.c(
+        {
+            gzip: true,
+            file: `react-nmi-collectjs-v${packageJson.version}.tgz`,
+        },
+        [
+            path.relative(projectRoot, path.join(projectRoot, 'lib')),
+            path.relative(projectRoot, path.join(projectRoot, 'package.json')),
+            path.relative(projectRoot, path.join(projectRoot, 'README.md'))
+        ]
+    )
+}
+
+Promise.resolve(true)
+    .then(() =>
+        Promise.all([
+             babel(esmRoot, 'esm'),
+        ]),
+    )
+    .then(() => {
+        createZip()
+    })
+    .catch((err) => {
+        if (err) console.error(err.stack || err.toString());
+        process.exit(1);
+    });
